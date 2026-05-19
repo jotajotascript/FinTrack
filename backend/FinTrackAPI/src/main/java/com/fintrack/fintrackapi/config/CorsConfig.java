08a170cb6bc9f40ccdc415f31284c4da0c8ff4fb
Danielly Moro <daniellymoro12@gmail.com>
2026-05-19 17:27:01 -0300

    fix: url vercel

--- backend/FinTrackAPI/src/main/java/com/fintrack/fintrackapi/config/CorsConfig.java
+++ backend/FinTrackAPI/src/main/java/com/fintrack/fintrackapi/config/CorsConfig.java
@@ -12,7 +12,7 @@ public class CorsConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedOrigins(
                     "http://localhost:4200",
-                    "https://fintrack-platform.vercel.app/"
+                    "https://fintrack-platform.vercel.app"
                 )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
